bd661a2351709d74345648b6ec1b798b4db04f85
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-22 18:11:37 +0600

    Update upload_path

--- file_server/models.py
+++ file_server/models.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+def upload_path(instance, filename):
+    return f'files/{instance.user.id}/{filename}'
+
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    file = models.FileField(upload_to=f'files/{user}')
+    file = models.FileField(upload_to=upload_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
