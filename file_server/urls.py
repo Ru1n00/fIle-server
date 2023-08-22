0c2eed0a4781f0ca9c07874fa32d625d660f8304
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-22 18:16:45 +0600

    Add urls

--- file_server/urls.py
+++ file_server/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
+app_name = 'file_server'
+
 urlpatterns = [
-    
-]
+    path('', views.index, name='index'),
+    path('sign_in', views.sign_in, name='login'),
+    path('sign_out', views.sign_out, name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
