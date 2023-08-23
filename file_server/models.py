29ef359c2e36171ecd20f623b446839f0859f1ac
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-23 10:06:13 +0600

    Auto fill name field

--- file_server/models.py
+++ file_server/models.py
@@ -8,10 +8,16 @@ def upload_path(instance, filename):
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
     file = models.FileField(upload_to=upload_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
         return self.name
+    
+
+    def save(self, *args, **kwargs):
+        if self.file:
+            self.name = self.file.name
+        super().save(*args, **kwargs)
