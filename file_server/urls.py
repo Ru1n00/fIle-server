9a2a8f373b8f0ad7063afecf4cdb175ac31b1439
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-08-23 08:03:38 +0600

    Protect media files

--- file_server/urls.py
+++ file_server/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sign_in', views.sign_in, name='login'),
     path('sign_out', views.sign_out, name='logout'),
+    re_path(r'media/*', views.protected_serve, {'document_root': settings.MEDIA_ROOT})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
